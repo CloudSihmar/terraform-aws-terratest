c47d5eddcc471d8d40492aa4ad0266b8924c918b
CloudSihmar <105888131+CloudSihmar@users.noreply.github.com>
2023-06-09 02:22:05 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,6 +7,7 @@ resource "aws_instance" "terraform-cloud-vm" {
   ami                    = var.ami
   instance_type          = var.instance_type
   subnet_id              = aws_subnet.subnet.id
+  associate_public_ip_address = true
   count                  = 1
   user_data              = file("install.sh")         
   vpc_security_group_ids = [aws_security_group.sg.id]
