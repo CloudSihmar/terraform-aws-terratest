06729c8babdb4e7034c6daf8ae59c32aa458f466
CloudSihmar <105888131+CloudSihmar@users.noreply.github.com>
2023-06-09 01:54:39 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,24 +1,53 @@
-module "vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
-  version = "3.0.0"
-
-  # VPC
-  name           = var.vpc_name
-  cidr           = var.vpc_cidr_block
-  azs            = var.vpc_availability_zones
-  public_subnets = var.vpc_public_subnets
+resource "aws_vpc" "vpc" {
+  cidr_block       = var.cidr
+  instance_tenancy = "default"
 
   tags = {
-    Department = "Prod"
+    Name = "main"
   }
-  vpc_tags = {
-    VPC_Region = var.region
-
-  }
-
-  #Tags - Subnets
-  public_subnet_tags = {
-    Subnet_Type = "Public Subnets"
-  }
-
 }
+
+resource "aws_subnet" "subnet" {
+  vpc_id     = aws_vpc.vpc.id
+  cidr_block = var.subnet
+ 
+
+  tags = {
+    Name = "Main"
+  }
+}
+
+resource "aws_route_table" "route" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  tags = {
+    Name = "example"
+  }
+}
+
+resource "aws_route_table_association" "main" {
+  subnet_id      = aws_subnet.subnet.id
+  route_table_id = aws_route_table.route.id
+}
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.vpc.id
+
+  tags = {
+    Name = "main"
+  }
+}
+
+output "vpc_id" {
+  value = aws_vpc.vpc.id
+}
+
+output "subnet_id" {
+  value = aws_subnet.subnet.id
+}
+
