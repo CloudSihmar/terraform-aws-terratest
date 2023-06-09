34f4bae28431a580d1a40c6ece3f0076c3635e06
CloudSihmar <105888131+CloudSihmar@users.noreply.github.com>
2023-06-09 15:17:41 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,25 +1,3 @@
-output "vpc_id" {
-  description = "VPC ID"
-  value       = module.vpc.vpc_id
-}
-
-
-output "vpc_cidr_block" {
-  description = "CIDR BLOCK OF VPC"
-  value       = module.vpc.vpc_cidr_block
-}
-
-
-output "public_subnets" {
-  description = "PUBLIC SUBNET LIST"
-  value       = module.vpc.public_subnets
-}
-
-
-output "azs" {
-  description = "LIST OF AVAILABILITY ZONES"
-  value       = module.vpc.azs
-}
 
 output "instance_id" {
 
